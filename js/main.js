f8b191f2a7b6ab3cb26d2c702836db6f24f037d6
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-11-02 12:42:26 -0800

    clears out console logs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 $(function() {
     $(".button").on("click", function( e ) {
         e.preventDefault();
-          console.log($($(this).attr('href')).offset().top);
-        
-          var px = ($($(this).attr('href')).offset().top);
-    
         $("body, html").animate({ 
-            scrollTop: px 
+            scrollTop: $($(this).attr('href')).offset().top 
         }, "slow");
         
     });
